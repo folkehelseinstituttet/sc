84fd69edce83e821fb033130602f2e05bb9eaf2b
Richard White <hello@rwhite.no>
2021-03-22 05:31:36 +0100

    change text columns to 400 characters

--- R/util_database.R
+++ R/util_database.R
@@ -433,7 +433,7 @@ create_table.default <- function(conn, table, fields, keys=NULL) {
 
 `create_table.Microsoft SQL Server` <- function(conn, table, fields, keys=NULL) {
   fields_new <- fields
-  fields_new[fields == "TEXT"] <- "NVARCHAR (50)"
+  fields_new[fields == "TEXT"] <- "NVARCHAR (400)"
   fields_new[fields == "DOUBLE"] <- "FLOAT"
   fields_new[fields == "BOOLEAN"] <- "BIT"
 
