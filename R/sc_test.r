1a65bdcdf1e58bb0e9340e70e86d2bc363101af4
Richard White <hello@rwhite.no>
2021-05-05 07:54:11 +0200

    sc_test

---
+++ R/sc_test.r
@@ -0,0 +1,165 @@
+# sc:::set_test_task()
+# sc::tm_run_task("sc_test")
+set_test_task <- function(){
+  # tm_run_task("sc_test")
+  add_task_from_config_v8(
+    name_grouping = "sc_test",
+    name_action = NULL,
+    name_variant = NULL,
+    cores = 2,
+    plan_argset_fn_name = NULL, # "PACKAGE::TASK_NAME_plan_argset"
+    for_each_plan = plnr::expand_list(
+      x = 1:30
+    ),
+    for_each_argset = NULL,
+    universal_argset = NULL,
+    upsert_at_end_of_each_plan = FALSE,
+    insert_at_end_of_each_plan = FALSE,
+    action_fn_name = "sc::sc_test_action",
+    data_selector_fn_name = "sc::sc_test_data_selector",
+    schema = list(
+    ),
+    info = "This task does..."
+  )
+
+}
+
+# **** action **** ----
+#' sc_test_action (action)
+#' @param data Data
+#' @param argset Argset
+#' @param schema DB Schema
+#' @export
+sc_test_action <- function(data, argset, schema) {
+  # tm_run_task("sc_test")
+
+  if(plnr::is_run_directly()){
+    # sc::tm_get_plans_argsets_as_dt("sc_test")
+
+    index_plan <- 1
+    index_argset <- 1
+
+    data <- sc::tm_get_data("sc_test", index_plan = index_plan)
+    argset <- sc::tm_get_argset("sc_test", index_plan = index_plan, index_argset = index_argset)
+    schema <- sc::tm_get_schema("sc_test")
+  }
+
+  # code goes here
+  # special case that runs before everything
+  if(argset$first_argset == TRUE){
+
+  }
+
+  Sys.sleep(1)
+
+  # put data in db table
+  # sc::fill_in_missing_v8(d, border = config$border)
+  # schema$SCHEMA_NAME$insert_data(d)
+  # schema$SCHEMA_NAME$upsert_data(d)
+  # schema$SCHEMA_NAME$drop_all_rows_and_then_upsert_data(d)
+
+  # special case that runs after everything
+  # copy to anon_web?
+  if(argset$last_argset == TRUE){
+    # sc::copy_into_new_table_where(
+    #   table_from = "anon_X",
+    #   table_to = "anon_webkht"
+    # )
+  }
+
+  return(TRUE)
+}
+
+# **** data_selector **** ----
+#' sc_test (data selector)
+#' @param argset Argset
+#' @param schema DB Schema
+#' @export
+sc_test_data_selector = function(argset, schema){
+  if(plnr::is_run_directly()){
+    # sc::tm_get_plans_argsets_as_dt("sc_test")
+
+    index_plan <- 1
+
+    argset <- sc::tm_get_argset("sc_test", index_plan = index_plan)
+    schema <- sc::tm_get_schema("sc_test")
+  }
+
+  # The database schemas can be accessed here
+  # d <- schema$SCHEMA_NAME$tbl() %>%
+  #   mandatory_db_filter(
+  #     granularity_time = NULL,
+  #     granularity_time_not = NULL,
+  #     granularity_geo = NULL,
+  #     granularity_geo_not = NULL,
+  #     country_iso3 = NULL,
+  #     location_code = NULL,
+  #     age = NULL,
+  #     age_not = NULL,
+  #     sex = NULL,
+  #     sex_not = NULL
+  #   ) %>%
+  #   dplyr::select(
+  #     granularity_time,
+  #     granularity_geo,
+  #     country_iso3,
+  #     location_code,
+  #     border,
+  #     age,
+  #     sex,
+  #
+  #     date,
+  #
+  #     isoyear,
+  #     isoweek,
+  #     isoyearweek,
+  #     season,
+  #     seasonweek,
+  #
+  #     calyear,
+  #     calmonth,
+  #     calyearmonth
+  #   )
+  #   dplyr::collect() %>%
+  #   as.data.table() %>%
+  #   setorder(
+  #     location_code,
+  #     date
+  #   )
+
+  # The variable returned must be a named list
+  retval <- list(
+    "NAME" = 1
+  )
+  retval
+}
+
+# **** plan_argset **** ----
+#' sc_test (plan/argset)
+#' This function can be deleted if you are not using "plan_argset_fn_name"
+#' inside sc::task_from_config_v3
+#' @export
+sc_test_plan_argset <- function(argset, schema) {
+  if(plnr::is_run_directly()){
+    argset <- sc::tm_get_argset("sc_test")
+    schema <- sc::tm_get_schema("sc_test")
+  }
+
+  # code goes here
+  for_each_plan <- plnr::expand_list(
+    x = 1
+  )
+
+  for_each_argset <- NULL
+
+  retval <- list(
+    for_each_plan = for_each_plan,
+    for_each_argset = for_each_argset
+  )
+}
+
+# **** functions **** ----
+
+
+
+
